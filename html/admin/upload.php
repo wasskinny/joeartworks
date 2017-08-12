643444a6ccc32d9b778e8f94eb68fd1ef6a5d232
Linn Thomas <wasskinny@gmail.com>
2017-08-12 12:54:28 -0700

    Another try

--- html/admin/upload.php
+++ html/admin/upload.php
@@ -30,7 +30,7 @@ class Upload {
 								if (!move_uploaded_file($_FILES["img_files"]["tmp_name"][$i], $filepath)) {
 									$msg .= "<p class='msg_error'>Failed to upload <strong>" . $filename . "</strong>.</p>";
 								} else {
-									$sqlUpload = "INSERT INTO images (`img_name`, `description`, `original`) VALUES ('$filename, '$file_name', '1') ";
+									$sqlUpload = "INSERT INTO images (`img_name`, `description`, `original`) VALUES ('$filename, '$file_name', '1'); ";
 									$result = mysqli_query($db, $sqlUpload);
 									
 									$msg .= "<p class='msg_success'><strong>" . $file_name . "</strong> uploaded successfully.</p>" ;
